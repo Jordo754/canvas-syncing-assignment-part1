c3136717583c6701b2e618ea24bf4da54948d42b
Jordan <jordo754@gmail.com>
2017-02-21 18:24:08 -0500

    Part 1 Done?

---
+++ src/server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/index.html`);
+
+const onRequest = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const app = http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
+
+// pass in the http server into socketio and grab the websocket server as io
+const io = socketio(app);
+
+let number = 0;
+
+const onJoined = (sock) => {
+  const socket = sock;
+
+  socket.join('lobby');
+};
+
+const onMsg = (sock) => {
+  const socket = sock;
+
+  socket.on('clientUpdate', (data) => {
+    number += data;
+
+    io.sockets.in('lobby').emit('serverUpdate', number);
+  });
+};
+
+io.sockets.on('connection', (socket) => {
+  console.log('started');
+
+  onJoined(socket);
+  onMsg(socket);
+});
+
+console.log('Websocket server started');
